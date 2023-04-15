26def672d4f5af316744223d2facf4a6ea0f037b
zincalex <92792074+zincalex@users.noreply.github.com>
2023-04-15 13:04:53 +0200

    steps 3.4, 4, 5 done. (Problem with the padding)

--- main.py
+++ main.py
@@ -4,45 +4,56 @@ import cv2
 import numpy as np
 import utilities as u
 
-N = 64
+N = 256
 R = random.randint(1,100) #range [1,100]
 
 def main() :
     #img_file = sys.arg[1]
-    sus = "img_test4.jpg"
+    sus = "img_test1.jpg"
     imgBGR = cv2.imread("image input/" + sus)
 
     #OpenCV show the image based on the BGR convention. In order to make things simpler to read, we procede 
     #converting BGR to RGB; once we want to show the image remember to convert it back
     imgRGB = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2RGB)
-    imgYCrCb = cv2.cvtColor(imgRGB, cv2.COLOR_RGB2YCR_CB) 
+    imgYCrCb = cv2.cvtColor(imgRGB, cv2.COLOR_RGB2YCrCb) 
     Y, Cr, Cb = cv2.split(imgYCrCb)
 
-    #cv2.imshow('RGB', imgBGR)
+    cv2.imshow('RGB', imgBGR)
     #cv2.imshow('Y', Y) 
-    cv2.imshow('Cb', Cb) 
+    #cv2.imshow('Cb', Cb) 
     #cv2.imshow('Cr', Cr) 
     
-
     Y_dct = u.my_dct(Y, N)
-    Cr_dct = u.my_dct(Cr, N)/255
-    Cb_dct = u.my_dct(Cb,N)/255
+    Cr_dct = u.my_dct(Cr, N)
+    Cb_dct = u.my_dct(Cb,N)
     
     #cv2.imshow('YDCT', Y_dct) 
-    cv2.imshow('CbDCT', Cb_dct) 
+    #cv2.imshow('CbDCT', Cb_dct) 
     #cv2.imshow('CrDCT', Cr_dct) 
-    
-    Y_idct = u.my_idct(Y_dct, N)
-    Cb_idct = u.my_idct(Cb_dct,N)
-    Cr_idct = u.my_idct(Cr_dct, N)
 
-    # convert to uint8
-    #Y_rebuilt = np.uint8(Y_idct)
-    Cb_rebuilt = np.uint8(Cb_idct)
-    Cr_rebuilt = np.uint8(Cr_idct)
+    #TODO ADD THIS %R FACTOR DONT KNOW WHAT TO DO YET
+    
+    Y_rebuilt = u.my_idct(Y_dct, N)
+    Cb_rebuilt = u.my_idct(Cb_dct,N)
+    Cr_rebuilt = u.my_idct(Cr_dct, N)
+
+    imgYCrCb_rebuilt = cv2.merge([Y_rebuilt, Cr_rebuilt, Cb_rebuilt])
+    out = cv2.cvtColor(imgYCrCb_rebuilt, cv2.COLOR_YCrCb2BGR)
+    cv2.imshow('asdfa', out)
+
+    MSE_Y = u.MSE(Y, Y_rebuilt)
+    MSE_Cb = u.MSE(Cb, Cb_rebuilt)
+    MSE_Cr = u.MSE(Cr, Cr_rebuilt)
+    MSE_P = u.MSE_P(MSE_Y, MSE_Cb, MSE_Cr)
+    PSNR = u.PNSR(MSE_P)
+
+
+    #TODO ITERATE THIS FOR FIRST TIME WITH RANDOM R, THEN BY INCREASING R FROM 10 TO 100
+    # IN 10 STEPS 
+    
 
     #cv2.imshow('Y rebuilt', Y_rebuilt) 
-    cv2.imshow('Cb rebuilt', Cb_rebuilt) 
+    #cv2.imshow('Cb rebuilt', Cb_rebuilt) 
     #cv2.imshow('Cr rebuilt', Cr_rebuilt) 
     
 
@@ -50,6 +61,28 @@ def main() :
     #cv2.imwrite('Y.png', Y) 
     #cv2.imwrite('onlyCb_as_bgr.png', onlyCb_as_bgr) 
     #cv2.imwrite('onlyCr_as_bgr.png', onlyCr_as_bgr)
+
+
+    """
+    test_faded_d_errors = np.array([
+    [255, 140, 216, 198, 180, 162, 144, 126],
+    [234, 216, 198, 180, 165, 144, 126, 108],
+    [216, 198, 170, 162, 144, 126, 108, 90],
+    [198, 180, 162, 144, 126, 108, 100, 72],
+    [180, 158, 144, 100, 108, 90, 72, 54],
+    [162, 144, 126, 108, 90, 72, 54, 18],
+    [144, 126, 108, 88, 72, 54, 0, 18],
+    [126, 108, 90, 72, 54, 36, 18, 0,]
+    ])
+
+    scale = 50
+    cv2.imshow('og', np.uint8(u.scale(test_faded_d_errors, scale)))
+    sus = u.my_dct(test_faded_d_errors, N)
+    #cv2.imshow('new', np.uint8(u.scale(sus, scale)))
+    sus_final = u.my_idct(sus, N)
+    print(sus_final)
+    cv2.imshow('new', np.uint8(u.scale(sus_final, scale)))
+    """
 
     cv2.waitKey(0) 
     cv2.destroyAllWindows()
--- utilities.py
+++ utilities.py
@@ -1,10 +1,11 @@
 import numpy as np
 import cv2 
+from math import log
 
 def my_dct(matrix, N) :
     height, lenght = matrix.shape  #number rows, number columns
     pad_l = lenght - ((lenght % N - N) if lenght%N != 0 else 0)
-    block_list = block_matrix_maker(matrix, N, height, lenght)
+    block_list = matrix_to_blocks(matrix, N, height, lenght)
     
     #Applying the dct transformation per block
     dct_block_list = []
@@ -12,36 +13,37 @@ def my_dct(matrix, N) :
         dct_block_list.append(cv2.dct(mtrx))
 
     #Building back the whole matrix
-    dct_mtrx = np.zeros((height, lenght))
-    for i in range(0, height) :
-        for j in range(0, lenght) : 
-            dct_mtrx[i][j] = dct_block_list[int((i - i%N) / N) * int(pad_l/N) + int((j - j%N) / N)][i%N][j%N]
-    return dct_mtrx
+    return blocks_to_matrix(dct_block_list, height, lenght, pad_l, N)
 
 
 
 def my_idct(matrix, N) :
     height, lenght = matrix.shape  #number rows, number columns
     pad_l = lenght - ((lenght % N - N) if lenght%N != 0 else 0)
-    block_list = block_matrix_maker(matrix, N, height, lenght) 
+    block_list = matrix_to_blocks(matrix, N, height, lenght) 
     
     #Applying the dct INVERTED transformation per block
     idct_block_list = []
     for mtrx in block_list : 
         idct_block_list.append(cv2.idct(mtrx))
 
-    #Building back the whole matrix 
-    #TODO MIGHT CONSIDER CREATING A SINGLE METHOD FOR THIS PART SINCE IS THE SAME IN DCT
-    idct_mtrx = np.zeros((height, lenght))
-    for i in range(0, height) :
-        for j in range(0, lenght) : 
-            idct_mtrx[i][j] = idct_block_list[int((i - i%N) / N) * int(pad_l/N) + int((j - j%N) / N)][i%N][j%N]
-
-    return idct_mtrx
+    #Building back the whole matrix, but returned the 8 bit unsigned integer version so the matrix
+    #can be immediatly used
+    return np.uint8(blocks_to_matrix(idct_block_list, height, lenght, pad_l, N))
 
 
 
-def block_matrix_maker(matrix, N, h, l) : 
+def blocks_to_matrix(blocklist, mtrx_h, mtrx_l, pad_l, N) : 
+    build_mtrx = np.zeros((mtrx_h, mtrx_l))
+    for i in range(0, mtrx_h) :
+        for j in range(0, mtrx_l) : 
+            build_mtrx[i][j] = blocklist[int((i - i%N) / N) * int(pad_l/N) + int((j - j%N) / N)][i%N][j%N]
+
+    return build_mtrx
+
+
+
+def matrix_to_blocks(matrix, N, h, l) : 
     fix_mtrx = padding(matrix, N, h, l)
     new_h, new_l = fix_mtrx.shape
     block_list = []
@@ -58,7 +60,38 @@ def block_matrix_maker(matrix, N, h, l) :
 
 
 
-#TODO EXPLAIN WHAT THIS METHOD DOES
 def padding(matrix, N, h, l) :
     Nbased_mtrx = np.pad(matrix, pad_width = ((0, (N - h % N) if h%N != 0 else 0), (0, (N - l % N) if l%N != 0 else 0)), mode = 'edge')
+    print(Nbased_mtrx)
     return Nbased_mtrx
+
+
+#Calcola l'MSE tra la componente originale e la componente "compressa" 
+def MSE (og_mtrx, compressed_mtrx) :
+    height, lenght = og_mtrx.shape
+    sque = 0
+    for i in range(0, height) :
+        for j in range(0, lenght) :
+            sque +=  (compressed_mtrx[i][j] - og_mtrx[i][j])**2
+    return sque / ((height * lenght))
+
+
+
+def MSE_P(MSE_Y, MSE_Cb, MSE_Cr) : return 0.75*MSE_Y + 0.125*MSE_Cb + 0.125*MSE_Cr
+
+
+def PNSR(MSE_P) : return 10 * log((255**2 / MSE_P), 10)
+
+
+
+#TODO REMEMBER TO DELETE THIS METHOD, JUST FOR TESTING PURPOSE
+def scale(matrix, mult):
+    height, width = matrix.shape
+    ret = np.zeros((height*mult, width*mult))
+    for i in range(0, height):
+        for j in range(0, width):
+            for k in range(0, mult):
+                for c in range(0,mult):
+                    ret[i*mult+k][j*mult+c] = matrix[i][j]
+
+    return ret
